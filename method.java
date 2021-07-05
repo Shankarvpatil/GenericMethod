b6de600fce3a9f0ddd856f086e858d07e8197131
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-07-05 22:08:41 +0530

    Update method.java

--- method.java
+++ method.java
@@ -1,18 +1,9 @@
 package com.Shankar.max;
 
-public class MaxFinder<E> {
-    E value1;
-    E value2;
-    E value3;
+public class MaxFinder {
 
-    public MaxFinder(E val1, E val2, E val3) {
-        this.value1 = val1;
-        this.value2 = val2;
-        this.value3 = val3;
-    }
-
-    public <E extends Comparable> E findMax(E value1, E value2, E value3) {
-        E max = value1;
+    public static <T extends Comparable> T findMax(T value1, T value2, T value3) {
+        T max = value1;
         if (value2.compareTo(max) > 0) {
             max = value2;
         }
@@ -22,5 +13,5 @@ public class MaxFinder<E> {
         System.out.printf("Maximum of %s %s %s is %s \n", value1, value2, value3, max);
         return max;
     }
-
+    
 }
